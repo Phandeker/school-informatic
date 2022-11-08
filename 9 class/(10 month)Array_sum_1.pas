76d7ea45de28748e6ca5e39a2873ccad8f5bc9de
Daniel Levin <74589933+Phandeker@users.noreply.github.com>
2022-11-08 11:55:32 +0300

    Create (10 month)Array_sum_1.pas

---
+++ 9 class/(10 month)Array_sum_1.pas
@@ -0,0 +1,13 @@
+program array_1; 
+  
+ var i, s: integer; 
+     ar: array [1..10] of integer; 
+  
+ begin  
+ s:=0;
+ for i := 1 to 10 do 
+ readln(a[i]); 
+ for i := 1 to 10 do 
+ s:=s+ar[i];
+ writeln(s);
+ end.
